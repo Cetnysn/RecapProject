f3560c5c8f46c0888e26ad6336cc5da10eac6b04
yasin çetin <cyasinn@hotmail.com>
2021-04-03 02:00:08 +0300

    9. Hafta Odev 1 Kayıt 2

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -9,20 +10,71 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EFCarDal());
-            //foreach (var car in carManager.GetAll())
-            //{
-            //    Console.WriteLine(car.Description);
-            //}
-            
-
-            //PersonelManager personelManager = new PersonelManager(new EFPersonelDal());
-            //foreach (var personel in personelManager.GetAll())
-            //{
-            //    Console.WriteLine("{0} / {1} / {2} ", personel.Id, personel.Name, personel.Surname);
-            //}
+            CarTest();
+            ColorTest();
+            BrandTest();
 
             Console.ReadLine();
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EFBrandDal());
+            Brand brand = new Brand
+            {
+                BrandId = 2,
+            };
+            brandManager.Add(brand);
+            foreach (var brands in brandManager.GetAllBrands())
+            {
+                Console.WriteLine(brands.BrandName);
+            }
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EFColorDal());
+            Color color = new Color
+            {
+                ColorName = "Java Green"
+            };
+            colorManager.Add(color);
+            foreach (var colors in colorManager.GetAllColors())
+            {
+                Console.WriteLine(colors.ColorName);
+            }
+            colorManager.Update(color);
+            Console.WriteLine("Renk güncellendi");
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EFCarDal());
+            Car car1 = new Car
+            {
+                CarId = 2
+            };
+
+            carManager.Add(new Car { BrandId = 1, CarId = 1, CarName = "Mercedes", ColorId = 1, DailyPrice = 329, ModelYear = 2013, Description = "Premium aile aracı" });
+            foreach (var cars in carManager.GetCarDetails())
+            {
+                Console.WriteLine("{0} / {1} / {2} / {3}", cars.CarName, cars.BrandName, cars.ColorName, cars.DailyPrice);
+            };
+
+            foreach (var cars in carManager.GetAllCars())
+            {
+                Console.WriteLine(cars.CarName);
+            }
+            foreach (var cars in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine(cars.DailyPrice);
+            }
+            foreach (var cars in carManager.GetCarsByColorId(5))
+            {
+                Console.WriteLine(cars.ModelYear);
+            }
+
+            carManager.Delete(car1);
         }
     }
 }
